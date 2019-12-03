9cae145a949253bdfd9bc00ea0b208b5cd66e2cb
Ricardo de Arruda <settiricardo@gmail.com>
2019-12-02 16:59:43 -0800

    create component selection

---
+++ src/components/selection.jsx
@@ -0,0 +1,31 @@
+import React,{useState} from 'react'
+
+//TODO: get x random sample test and weight by score (hight score higher chance to be selected) and select y percentage and fill with random z times 
+
+
+const Selection = () => {
+    const [sampleSize, setSampleSize] = useState(100)
+    const [seletionPercetage, setSelectionPercetage] = useState(0.1)
+    const [iteration, setIteration] = useState(20)
+
+    return <div></div>
+}
+
+
+export default Section
+
+
+
+
+
+
+
+
+
+function genRobot(){
+    
+
+}
+
+
+
